ed9008f1b22d6d80810441c38e20738b565905e7
Ltcc0 <3381983963@qq.com>
2025-09-18 21:55:26 +0800

    subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SuperStructure/IntakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SuperStructure/IntakeSubsystem.java
@@ -1,32 +1,33 @@
 package org.firstinspires.ftc.teamcode.subsystems.SuperStructure;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-import java.util.Optional;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class IntakeSubsystem extends SubsystemBase{
-    private final LinearMotion intake;
+    public  Motor intakeMotor;
+    private Telemetry telemetry;
+    private  DcMotor encoder;
+    private boolean isMotorInverted = false;
+    private double error = 0, lastError = 0, integral = 0, derivative = 0;
+    public static double Kp = 0, Ki = 0, Kd = 0, power;
+    private ElapsedTime timer = new ElapsedTime();
 
-    public IntakeSubsystem(HardwareMap hardwareMap){
-        this.intake = new LinearMotion(
-                "IntakeMotor",
-                new DcMotor[]{hardwareMap.get(DcMotor.class,"intake")},
-                new boolean[]{true},
-                hardwareMap.get(DcMotorEx.class,"intake"),
-                true,
-                Optional.empty(),
-                1800,
-                0,
-                0,
-                0,
-                0);
+    public void init(HardwareMap hardwareMap){
+        intakeMotor = hardwareMap.get(MotorEx.class,"intakeMotor");
+        intakeMotor.setInverted(isMotorInverted);
+        intakeMotor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        encoder = hardwareMap.get(DcMotor.class,"intakeMotor");
     }
 
-    public void periodic(){
-
+    public void setIntakePower(){
+        
     }
+
 
 }
