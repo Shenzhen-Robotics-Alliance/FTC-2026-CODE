4f614354ece11d26584f4f6b89e8562481540063
ikun-jntm <1273582130@qq.com>
2025-10-03 17:44:52 +0800

    AprilTag following with PID controller test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ApriltagFollowTest.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.tests;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.teamcode.subsystems.vision.VisionSubsystem;
+@TeleOp(name = "Apriltag Follow Test")
+public class ApriltagFollowTest extends OpMode {
+    private DcMotor RotMotor;
+    @Override
+    public void init() {
+        RotMotor = hardwareMap.get(DcMotor.class, "RotMotor");
+
+    }
+
+    @Override
+    public void loop() {
+        VisionSubsystem limelight = new VisionSubsystem(hardwareMap);
+
+        limelight.setPipeline(0);
+
+        telemetry.setMsTransmissionInterval(11);
+        telemetry.addData("Has Target", "Press A to Switch Pipeline");
+        telemetry.addLine("---");
+
+        telemetry.addData("Target Found", () -> limelight.hasTarget());
+        telemetry.addData("Target ID", () -> limelight.getTargetID());
+        telemetry.addData("Target X", () -> String.format("%.2f", limelight.getTargetX()));
+        telemetry.addData("Target Y", () -> String.format("%.2f", limelight.getTargetY()));
+        RotMotor.setPower(limelight.getTargetX()*-0.3);
+
+    }
+}
