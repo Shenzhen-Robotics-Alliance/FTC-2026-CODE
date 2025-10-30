610c722773c173da5c9668aa2d252bb3e915485b
Ltcc0 <3381983963@qq.com>
2025-10-30 16:53:42 +0800

    Create ShortPoint.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/ShortPoint.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.autos;
+
+import com.arcrobotics.ftclib.command.Command;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+
+import org.firstinspires.ftc.teamcode.RobotContainer;
+
+public class ShortPoint implements Auto{
+    @Override
+    public Command getAutonomousCommands(RobotContainer robotContainer) {
+        final SequentialCommandGroup sequence = new SequentialCommandGroup();
+
+        return null;
+
+    }
+}
