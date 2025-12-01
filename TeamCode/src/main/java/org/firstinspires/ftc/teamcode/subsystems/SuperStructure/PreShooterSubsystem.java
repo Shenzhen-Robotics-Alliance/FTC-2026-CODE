76e2f263a351785ec88452df85ba83d04a6e3991
ikun-jntm <1273582130@qq.com>
2025-12-01 13:58:58 +0800

    Implemented a new PreShooter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SuperStructure/PreShooterSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SuperStructure/PreShooterSubsystem.java
@@ -7,22 +7,27 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class PreShooterSubsystem {
-    public final Servo preShooter;
+    public final Servo preShooter1;
+    public final Servo preShooter2;
 
     public PreShooterSubsystem(HardwareMap hardwareMap){
-        this.preShooter = hardwareMap.get(Servo.class,"PreShooter");
+        this.preShooter1 = hardwareMap.get(Servo.class,"PreShooter1");
+        this.preShooter2 = hardwareMap.get(Servo.class,"PreShooter2");
     }
 
     public Command setShootingAngle(){
         return new RunCommand(() ->
-                preShooter.setPosition(0.58)
+        {preShooter1.setPosition(0.58);
+         preShooter2.setPosition(0.655);
+        }
         );
      }
 
     public Command setStopPreShooter(){
         return new RunCommand(() ->
-                preShooter.setPosition(0.655));
+        {preShooter1.setPosition(0.655);
+         preShooter2.setPosition(0.58);
+        }
+        );
     }
-
-    
 }
