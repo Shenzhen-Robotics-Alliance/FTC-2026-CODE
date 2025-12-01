c873f24cde655fbbb6a69f7b792f67780ba0c716
Ltcc0 <3381983963@qq.com>
2025-12-01 20:10:52 +0800

    new

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpRobot.java
@@ -88,18 +88,16 @@ public class TeleOpRobot extends Robot {
 
         this.copilotGamePad.getGamepadButton(GamepadKeys.Button.LEFT_BUMPER)
                 .whenActive(() -> isAutoShootingMode = true);
-
-
-
+        
         new Trigger(() -> pilotGamePad.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) > 0.5)
-                .whileActiveContinuous(robotContainer.intakeCommand.intakeContinuously());
+                .whileActiveOnce(robotContainer.intakeCommand.intakeContinuously());
 
         //pilot use A to control intake
         this.pilotGamePad.getGamepadButton(GamepadKeys.Button.A)
-                .whenPressed(robotContainer.intakeCommand.outtakeContinuously());
+                .whenHeld(robotContainer.intakeCommand.outtakeContinuously());
 
         new Trigger(() -> pilotGamePad.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER) > 0.5)
-                .whileActiveContinuous(
+                .whileActiveOnce(
                         new SelectCommand(
                                 new HashMap<Object, Command>() {{
                                     put(false, robotContainer.shootCommand.fixShootShortContinuously());
